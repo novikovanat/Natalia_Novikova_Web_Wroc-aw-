f9c245307ff0648c3dea88c2bc8c2a1e3bfd97da
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-10 15:34:42 +0200

    navigation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import Navigation from '../Navigation/Navigation.tsx';
+const NotFoundPage = lazy(() => import("../../pages/NotFoundPage.tsx"));
 const ProductPage = lazy(() => import("../../pages/ProductPage.tsx"));
 const BasketPage = lazy(() => import("../../pages/BasketPage.tsx"));
-const OrderSummaryPage = lazy(() =>
-  import("../../pages/OrderSummaryPage.tsx"))
-const OderConfirmationPage = lazy(() => import("../../pages/OderConfirmationPage.tsx"));
+const OrderSummaryPage = lazy(() => import("../../pages/OrderSummaryPage.tsx"));
+const OderConfirmationPage = lazy(
+  () => import("../../pages/OderConfirmationPage.tsx")
+);
 
-
-
-
-import './App.css'
 
 function App() {
   return (
-    <Suspense fallback='wait ...for it'>
-      <Routes>
-        <Route path="/" element={<ProductPage />} />
-        <Route path="/basket" element={<BasketPage />} />
-        <Route path="/basket/summary" element={<OrderSummaryPage />}/>
-        <Route path="/confirmation" element={<OderConfirmationPage />}/>
-      </Routes>
-    </Suspense>
+    <>
+      <Navigation/>
+      <Suspense fallback="wait ...for it">
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/basket/summary" element={<OrderSummaryPage />} />
+          <Route path="/confirmation" element={<OderConfirmationPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
-
-export default App
+export default App;
