b1b64b2aa73bc2c990a75d7d69b37c1fc85987bc
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-12 04:54:42 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Natalia_Novikova_Web_Wroc-aw-/",
+  base: "/",
   build: {
     sourcemap: true,
   },
